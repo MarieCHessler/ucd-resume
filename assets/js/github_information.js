008c60b2019b63ca2c84b5e91379bc5102c77b0a
MarieCHessler <marie@hessler.se>
2022-03-24 14:18:07 +0000

    Add fetchGitHubInformation function with response and error code to github_information

--- assets/js/github_information.js
+++ assets/js/github_information.js
@@ -10,4 +10,19 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) { // Response comes from getJSON method
+            var userData = response; // Response is stored in userData
+            $("#gh-user-data").html(userInformationHTML(userData)); // jQuery selector selects gh-user-data div and set HTML to results of userInformationHTML
+        }, function(errorResponse) {
+            if (errorResponse.status === 404) { // 404 is a not found error
+                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`); // Set gh-user-data div's HTML to error message
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
